a5df685e5174710c48b2bfba5595026805dee61d
Justy Oberg <justyoberg1@gmail.com>
2023-04-04 13:31:44 -0700

    Finish exercise 3.2

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ app.get('/api/persons', (request, response) => {
   response.json(data.numbers)
 })
 
+app.get('/info', (request, response) => {
+  const time = new Date()
+  response.send(`
+  <p>Phonebook has info for ${data.numbers.length} people</p>
+  <p>${time}</p>
+  `)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
