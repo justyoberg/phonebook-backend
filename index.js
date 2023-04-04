b0e0060837311f03e37a4a63f7f102cbd237aa88
Justy Oberg <justyoberg1@gmail.com>
2023-04-04 14:27:54 -0700

    Finish exercise 3.5

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const data = require('./data')
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 app.get('/api/persons', (request, response) => {
   response.json(data.numbers)
 })
@@ -23,6 +25,34 @@ app.get('/info', (request, response) => {
   <p>Phonebook has info for ${data.numbers.length} people</p>
   <p>${time}</p>
   `)
+})
+
+const generateId = () => {
+  return Math.floor(Math.random() * 999999)
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  
+  if (!body.name || !body.number) {
+    return body.name ? 
+      response.status(400).json({
+        error: 'number missing'
+      }) : 
+      response.status(400).json({
+        error: 'name missing'
+      })
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  }
+
+  data.numbers = data.numbers.concat(person)
+
+  response.json(data.numbers)
 })
 
 app.delete('/api/persons/:id', (request, response) => {
